abb6821a0c76152161c6e0edcbe63205b94a7f7a
katekaa <katrinne253@gmail.com>
2022-11-08 18:49:42 +0300

    fixes from 1 hw

--- askme_chestnova/app/models.py
+++ askme_chestnova/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+QUESTIONS = [
+    {
+        'id': question_id,
+        'title': f'Question {question_id}',
+        'text': f'Text of question #{question_id}',
+        'answers_number': question_id*question_id,
+        'tags': ['tag1' for i in range(question_id)]
+    }
+   for question_id in range(10)
+   
+]
--- askme_chestnova/app/views.py
+++ askme_chestnova/app/views.py
@@ -2,9 +2,11 @@ PYTHONPATH="D:\vk_ed\vk-web\askme_chestnova\venv\lib\site-packages"
 
 
 from django.shortcuts import render
+from . import models
 
 def index(request):
-    return render(request, 'index.html')
+    context = {'questions': models.QUESTIONS}
+    return render(request, 'index.html', context=context)
 
 def question(request, question_id: int ):
     return render(request, 'question.html')
